397a60bb14e38b924cf15cafc8523a71fb1d272b
Rohit Sadhu <rohitsadhupersonal@gmail.com>
2022-08-25 16:38:04 +0530

    third commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
